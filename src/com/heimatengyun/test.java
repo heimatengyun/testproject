30da4bc6cdb2c75a5d3719db7a9400b77d31745b
heimatengyun <heimatengyun@qq.com>
2019-04-23 13:30:05 +0800

    Initial commit

---
+++ src/com/heimatengyun/test.java
@@ -0,0 +1,18 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: heimatengyun
+ * Date: 2019-04-23
+ * Time: 13:20
+ */
+package com.heimatengyun;
+
+/**
+ @author heimatengyun
+ @create 2019-04-23 13:20
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
